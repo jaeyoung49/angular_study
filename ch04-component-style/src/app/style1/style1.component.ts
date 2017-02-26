3df2d8150dc5c961023f47eea396d0756ff09a57
Kelab.Lee (이재영) <jaeyoung49@gmail.com>
2017-02-26 18:42:41 +0900

    Angular Stydy Examples
    ch04_Component
    - input-decorator
    - hierarchy
    - call-search-element
    - style

---
+++ ch04-component-style/src/app/style1/style1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'style-component',
+  template: `<span>Hello</span>`,
+  styles: ['span { font-style: italic; font-size: 50px }']
+})
+export class StyleComponent {
+
+}
